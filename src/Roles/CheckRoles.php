62f8dcb0b3bebbd06e533987193a01420c3434e0
Manfred Parbatia <superflyman90@gmail.com>
2021-01-24 11:26:23 +0100

    Front-end rajout de postcss+ sass et sass loader

--- src/Roles/CheckRoles.php
+++ src/Roles/CheckRoles.php
@@ -12,17 +12,17 @@ class CheckRoles
     public function __construct(Security $security)
     {
         $this->security = $security;
-
     }
 
 
-    public function checkroles(){
-    $user = $this->security->getUser();
-    if ($user->getRoles()==["ROLE_ADMIN"]){
-        $route = 'app_admin_index';
-    }else if($user->getRoles()==["ROLE_USER"]){
-        $route ='app_user_index';
+    public function checkroles()
+    {
+        $user = $this->security->getUser();
+        if ($user->getRoles() == ["ROLE_ADMIN"]) {
+            $route = 'app_admin_index';
+        } else if ($user->getRoles() == ["ROLE_USER"]) {
+            $route = 'app_user_index';
+        }
+        return $route;
     }
-    return $route ;
-}
 }
